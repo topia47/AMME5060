aaf9f0c24fe7a08fcbfe04ebe1f0777b0985012a
Sanju Jacob <sanjujacob47@gmail.com>
2019-08-23 16:01:56 +1000

    Add files via upload

---
+++ tut2/mpitute1.f90
@@ -0,0 +1,25 @@
+! Template to start MPI Program
+! Tute 1 Problem 1
+
+PROGRAM TUTE2Q1
+	IMPLICIT NONE
+	INCLUDE 'mpif.h'  ! Include MPI librarires
+	
+	! Variable declartion
+
+		
+  ! Initialize MPI Program
+  
+  
+  ! Get Size of Communicator (Number of procs)
+  
+  
+  ! Get Rank of Communicator (proc id)
+	
+
+	! Write Out Information from each processor
+
+	
+	! Finalize MPI program
+	
+END PROGRAM TUTE2Q1
